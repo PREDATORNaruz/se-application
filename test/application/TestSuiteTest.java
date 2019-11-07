d1393dea42f50ed0a456cbb36d88a1e4cc4db772
PREDATORNaruz <56965724+PREDATORNaruz@users.noreply.github.com>
2019-11-07 14:52:27 +0100

    Add files via upload

---
+++ test/application/TestSuiteTest.java
@@ -0,0 +1,14 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestSuiteTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/application/model/CustomerTestTest.java
@@ -0,0 +1,14 @@
+package application.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerTestTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
